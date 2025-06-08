9bc018a59d83bd0e2c893ddab620f5cb08df0e00
Serhii Polishevskyi <v-spolishevskyi@axsome.com>
2025-06-08 17:49:38 +0300

    Add TransferMoneyUserTest positive case

--- src/test/java/iteration1/TransferMoneyUserTest.java
+++ src/test/java/iteration1/TransferMoneyUserTest.java
@@ -1,13 +1,19 @@
 package iteration1;
 
+import models.AccountsResponse;
 import models.CreateUserRequest;
 import models.DepositRequest;
-import models.DepositResponse;
 import models.TransferMoneyRequest;
 import org.junit.jupiter.api.Test;
+import requests.skelethon.Endpoint;
+import requests.skelethon.requesters.CrudRequester;
+import requests.skelethon.requesters.ValidatedCrudRequester;
 import requests.steps.AdminSteps;
-import requests.steps.UserSteps;
+import specs.RequestSpecs;
 import specs.ResponseSpecs;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
 
 public class TransferMoneyUserTest extends BaseTest {
     private static final float INITIAL_DEPOSIT = 1000.0f;
@@ -16,22 +22,48 @@ public class TransferMoneyUserTest extends BaseTest {
     @Test
     public void userCanTransferMoneyWithCorrectData() {
         CreateUserRequest userRequest = AdminSteps.createUser();
-//        DepositResponse sourceAccount = UserSteps.createAccount(userRequest);
-//        DepositResponse targetAccount = UserSteps.createAccount(userRequest);
-//
-//        DepositRequest depositRequest = DepositRequest.builder()
-//                .id(sourceAccount.getId())
-//                .balance(INITIAL_DEPOSIT)
-//                .build();
-//
-//        UserSteps.depositMoneyToAccount(depositRequest, userRequest, ResponseSpecs.requestReturnsOK());
-//
-//        TransferMoneyRequest transferRequest = TransferMoneyRequest.builder()
-//                .senderAccountId(sourceAccount.getId())
-//                .receiverAccountId(targetAccount.getId())
-//                .amount(TRANSFER_AMOUNT)
-//                .build();
-//
-//        UserSteps.transferMoneyToAccount(transferRequest, userRequest, ResponseSpecs.requestReturnsOK());
+
+        AccountsResponse sourceAccount = new ValidatedCrudRequester<AccountsResponse>(
+                RequestSpecs.authAsUserSpec(userRequest.getUsername(), userRequest.getPassword()),
+                Endpoint.ACCOUNTS,
+                ResponseSpecs.entityWasCreated())
+                .post(null);
+
+        AccountsResponse targetAccount = new ValidatedCrudRequester<AccountsResponse>(
+                RequestSpecs.authAsUserSpec(userRequest.getUsername(), userRequest.getPassword()),
+                Endpoint.ACCOUNTS,
+                ResponseSpecs.entityWasCreated())
+                .post(null);
+
+        DepositRequest depositRequest = DepositRequest.builder()
+                .id(sourceAccount.getId())
+                .balance(INITIAL_DEPOSIT)
+                .build();
+
+        new CrudRequester(
+                RequestSpecs.authAsUserSpec(userRequest.getUsername(), userRequest.getPassword()),
+                Endpoint.DEPOSIT,
+                ResponseSpecs.requestReturnsOK())
+                .post(depositRequest);
+
+        TransferMoneyRequest transferRequest = TransferMoneyRequest.builder()
+                .senderAccountId(sourceAccount.getId())
+                .receiverAccountId(targetAccount.getId())
+                .amount(TRANSFER_AMOUNT)
+                .build();
+
+        new CrudRequester(
+                RequestSpecs.authAsUserSpec(userRequest.getUsername(), userRequest.getPassword()),
+                Endpoint.TRANSFER,
+                ResponseSpecs.requestReturnsOK())
+                .post(transferRequest);
+
+        new CrudRequester(RequestSpecs.authAsUserSpec(userRequest.getUsername(), userRequest.getPassword()),
+                Endpoint.TRANSACTIONS,
+                ResponseSpecs.requestReturnsOK())
+                .get(depositRequest.getId())
+                .body("$", hasSize(2))
+                .body("find { it.type == 'DEPOSIT' }.amount", equalTo(depositRequest.getBalance()))
+                .body("find { it.type == 'TRANSFER_OUT' }.amount", equalTo(transferRequest.getAmount()));
     }
 }
