015db73871eda4dd768f9e21d8716d42730dc803
Serhii Polishevskyi <v-spolishevskyi@axsome.com>
2025-06-08 18:13:20 +0300

    Add TransferMoneyUserTest negative case

--- src/test/java/iteration1/TransferMoneyUserTest.java
+++ src/test/java/iteration1/TransferMoneyUserTest.java
@@ -5,12 +5,17 @@ import models.CreateUserRequest;
 import models.DepositRequest;
 import models.TransferMoneyRequest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import requests.skelethon.Endpoint;
 import requests.skelethon.requesters.CrudRequester;
 import requests.skelethon.requesters.ValidatedCrudRequester;
 import requests.steps.AdminSteps;
 import specs.RequestSpecs;
 import specs.ResponseSpecs;
+
+import java.util.stream.Stream;
 
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
@@ -65,5 +70,54 @@ public class TransferMoneyUserTest extends BaseTest {
                 .body("$", hasSize(2))
                 .body("find { it.type == 'DEPOSIT' }.amount", equalTo(depositRequest.getBalance()))
                 .body("find { it.type == 'TRANSFER_OUT' }.amount", equalTo(transferRequest.getAmount()));
+    }
+
+    private static Stream<Arguments> invalidTransferData() {
+        return Stream.of(Arguments.of(0.0f, "Invalid transfer: insufficient funds or invalid accounts"),
+                Arguments.of(-50.0f, "Invalid transfer: insufficient funds or invalid accounts"),
+                Arguments.of(999999.0f, "Transfer amount cannot exceed 10000"));
+    }
+
+    @ParameterizedTest
+    @MethodSource("invalidTransferData")
+    public void userCannotTransferMoneyWithInvalidData(Float amount, String errorMessage) {
+        CreateUserRequest userRequest = AdminSteps.createUser();
+
+        AccountsResponse sourceAccount = new ValidatedCrudRequester<AccountsResponse>(
+                RequestSpecs.authAsUserSpec(userRequest.getUsername(), userRequest.getPassword()),
+                Endpoint.ACCOUNTS,
+                ResponseSpecs.entityWasCreated())
+                .post(null);
+
+        DepositRequest depositRequest = DepositRequest.builder()
+                .id(sourceAccount.getId())
+                .balance(INITIAL_DEPOSIT)
+                .build();
+
+        new CrudRequester(
+                RequestSpecs.authAsUserSpec(userRequest.getUsername(), userRequest.getPassword()),
+                Endpoint.DEPOSIT,
+                ResponseSpecs.requestReturnsOK())
+                .post(depositRequest);
+
+        TransferMoneyRequest transferRequest = TransferMoneyRequest.builder()
+                .senderAccountId(sourceAccount.getId())
+                .receiverAccountId(sourceAccount.getId())
+                .amount(amount)
+                .build();
+
+        new CrudRequester(
+                RequestSpecs.authAsUserSpec(userRequest.getUsername(), userRequest.getPassword()),
+                Endpoint.TRANSFER,
+                ResponseSpecs.requestReturnsBadRequest(errorMessage))
+                .post(transferRequest);
+
+        new CrudRequester(
+                RequestSpecs.authAsUserSpec(userRequest.getUsername(), userRequest.getPassword()),
+                Endpoint.TRANSACTIONS,
+                ResponseSpecs.requestReturnsOK())
+                .get(sourceAccount.getId())
+                .body("$", hasSize(1))
+                .body("find { it.type == 'DEPOSIT' }.amount", equalTo(depositRequest.getBalance()));
     }
 }
