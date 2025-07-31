8b0b0a6aa0cdf32cf38d9dc4e76a2a862604f876
Serhii Polishevskyi <v-spolishevskyi@axsome.com>
2025-07-31 13:19:50 +0300

    Debug CI pipeline for running tests

--- src/main/java/common/extensions/TimingExtension.java
+++ src/main/java/common/extensions/TimingExtension.java
@@ -1,5 +1,6 @@
 package common.extensions;
 
+import org.junit.jupiter.api.extension.AfterAllCallback;
 import org.junit.jupiter.api.extension.AfterTestExecutionCallback;
 import org.junit.jupiter.api.extension.BeforeTestExecutionCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
@@ -7,20 +8,27 @@ import org.junit.jupiter.api.extension.ExtensionContext;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TimingExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback {
+public class TimingExtension implements BeforeTestExecutionCallback, AfterTestExecutionCallback, AfterAllCallback {
     private Map<String, Long> startTimes = new HashMap<>();
+    private Map<String, Long> durationTimes = new HashMap<>();
+
 
     @Override
     public void beforeTestExecution(ExtensionContext extensionContext) throws Exception {
-        String testName = extensionContext.getRequiredTestClass().getPackageName() + "." + extensionContext.getDisplayName();
-        startTimes.put(testName, System.currentTimeMillis());
-        System.out.println("Thread " + Thread.currentThread().getName() + ": Test started " + testName);
+        startTimes.put(extensionContext.getRequiredTestClass().getPackageName() + extensionContext.getDisplayName(), System.currentTimeMillis());
     }
 
     @Override
     public void afterTestExecution(ExtensionContext extensionContext) throws Exception {
-        String testName = extensionContext.getRequiredTestClass().getPackageName() + "." + extensionContext.getDisplayName();
-        Long testDuration = System.currentTimeMillis() - startTimes.get(testName);
-        System.out.println("Thread " + Thread.currentThread().getName() + ": Test finished " + testName + ", test duration " + testDuration + " ms");
+        long startTime = startTimes.get(extensionContext.getRequiredTestClass().getPackageName() + extensionContext.getDisplayName());
+        long duration = System.currentTimeMillis() - startTime;
+        durationTimes.put(extensionContext.getRequiredTestClass().getPackageName() + extensionContext.getDisplayName(), duration);
+    }
+
+    @Override
+    public void afterAll(ExtensionContext extensionContext) throws Exception {
+        durationTimes.forEach( (testName, duration) ->
+                System.out.println("Test '" + testName + "' took " + duration + " ms")
+        );
     }
 }
