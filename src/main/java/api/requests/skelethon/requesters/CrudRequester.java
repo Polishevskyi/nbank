8b12f7be6ebb8599585d79a3c1da7cc66b0186cd
Serhii Polishevskyi <v-spolishevskyi@axsome.com>
2025-08-04 15:03:46 +0300

    Add report pipeline

--- src/main/java/api/requests/skelethon/requesters/CrudRequester.java
+++ src/main/java/api/requests/skelethon/requesters/CrudRequester.java
@@ -1,11 +1,13 @@
 package api.requests.skelethon.requesters;
 
+import api.configs.Config;
 import api.models.BaseModel;
 import api.requests.skelethon.Endpoint;
 import api.requests.skelethon.HttpRequest;
 import api.requests.skelethon.interfaces.CrudEndpointInterface;
 import api.requests.skelethon.interfaces.GetAllEndpointInterface;
 import common.helpers.StepLogger;
+import io.qameta.allure.Step;
 import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
@@ -13,6 +15,8 @@ import io.restassured.specification.ResponseSpecification;
 import static io.restassured.RestAssured.given;
 
 public class CrudRequester extends HttpRequest implements CrudEndpointInterface, GetAllEndpointInterface {
+    private final static String API_VERSION = Config.getProperty("apiVersion");
+
     public CrudRequester(RequestSpecification requestSpecification, Endpoint endpoint, ResponseSpecification responseSpecification) {
         super(requestSpecification, endpoint, responseSpecification);
     }
@@ -24,7 +28,7 @@ public class CrudRequester extends HttpRequest implements CrudEndpointInterface,
             return given()
                     .spec(requestSpecification)
                     .body(body)
-                    .post(endpoint.getUrl())
+                    .post(API_VERSION + endpoint.getUrl())
                     .then()
                     .assertThat()
                     .spec(responseSpecification);
@@ -32,50 +36,55 @@ public class CrudRequester extends HttpRequest implements CrudEndpointInterface,
     }
 
     @Override
+    @Step("GET request to {endpoint} with id {id}")
     public ValidatableResponse get(long id) {
         return given()
                 .spec(requestSpecification)
-                .get(endpoint.getUrl().replace("{accountId}", String.valueOf(id)))
+                .get(API_VERSION + endpoint.getUrl().replace("{accountId}", String.valueOf(id)))
                 .then()
                 .assertThat()
                 .spec(responseSpecification);
     }
 
+    @Step("GET request to {endpoint}")
     public ValidatableResponse get() {
         return given()
                 .spec(requestSpecification)
-                .get(endpoint.getUrl())
+                .get(API_VERSION + endpoint.getUrl())
                 .then()
                 .assertThat()
                 .spec(responseSpecification);
     }
 
     @Override
+    @Step("PUT request to {endpoint} with body {model}")
     public ValidatableResponse put(BaseModel model) {
         return given()
                 .spec(requestSpecification)
                 .body(model)
-                .put(endpoint.getUrl())
+                .put(API_VERSION + endpoint.getUrl())
                 .then()
                 .assertThat()
                 .spec(responseSpecification);
     }
 
     @Override
+    @Step("DELETE request for {endpoint} with id {id}")
     public ValidatableResponse delete(long id) {
         return given()
                 .spec(requestSpecification)
-                .delete(endpoint.getUrl().replace("{accountId}", String.valueOf(id)))
+                .delete(API_VERSION + endpoint.getUrl().replace("{accountId}", String.valueOf(id)))
                 .then()
                 .assertThat()
                 .spec(responseSpecification);
     }
 
     @Override
+    @Step("GET request to {endpoint}")
     public ValidatableResponse getAll(Class<?> clazz) {
         return given()
                 .spec(requestSpecification)
-                .get(endpoint.getUrl())
+                .get(API_VERSION + endpoint.getUrl())
                 .then().assertThat()
                 .spec(responseSpecification);
     }
